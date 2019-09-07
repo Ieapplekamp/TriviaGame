385b3998c47ca5e52175cd86088f6829e74c0a2a
Ian Applekamp <ieapplekamp@gmail.com>
2019-09-06 20:47:24 -0400

    added a few variables and html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,8 @@
-// ugh I don't even know where to start with this one.. I hope this shit clicks soon cause its not
 // https://www.usefultrivia.com/music_trivia/index_iii.html
-// psuedo code ... im just tired i wanna bitch
-
-// so we gotta
+// manipulate the dom manipulate the dom manipulate the dom
 
 // Start button - something the user clicks/presses to start the code, not much else on the screen
+
 // once pressed, the screen will change to the game itself
 // the game will start a timer (fuck timers man) immediately (probably like a minute and a half would be best depending on the questions)
 // there will be a bunch of questions (do the fucking hard one man just fucking do it, make the god damn screen dynamic n shit just fuckin do it)
@@ -15,15 +13,20 @@
 // somethings got to change the screen and display totals and shit once the timer is out 
 // going to need something to start the next game - ya fucking reset(bitch)
 
-// alright so what kinda variables do i need
-// need that function document start thingy too
-
 $(document).ready(function() {
-    console.log('hi');
 
+    // alright so what kinda variables do i need.. think objects? :/ 
     var wins = 0;
     var losses = 0;
     var unanswered = 0;
+
+    // an onLoad() kind of function to change the DOM
+    //this is probably going to have to go in the start function becuase I want the page to dynamically change on click
+
+    function home() {
+        // i want this to be how the home screen will display for the user. it wont be used more than once maybe
+    }
+
     // possible questions
     var questions = ["Which Johnny Cash song did an advertising company want to use for a hemorrhoids ad?",
         "What was the band known as Linkin Park originally called?",
@@ -34,19 +37,21 @@ $(document).ready(function() {
         "What band was originally named Tony Flow and the Miraculously Majestic Masters of Mayhem?"];
 
     // an idea
-    var correctAnwers = ['Ring Of Fire', //
-        'Xero', //
-        'American Pie', //
-        'Birth Control', //
-        '27', //
-        'The Time', //
-        'Red Hot Chili Peppers' //
-    ] 
+    var correctAnwers = ['Ring Of Fire',
+        'Xero',
+        'American Pie',
+        'Birth Control',
+        '27',
+        'The Time',
+        'Red Hot Chili Peppers']; 
 
     randomQuestion = questions[Math.floor(Math.random() * questions.length)];
 
     $('#questions').append(randomQuestion);
     console.log(randomQuestion);
+
+
+    // --------------------- stop watch attempt --------------------- \\
 
 
 })
